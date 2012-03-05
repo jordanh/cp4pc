06206b4a1d21bfe67f45e2c3c86c80dc16ae504d
Tom Collins <tom.collins@digi.com>
2012-03-05 15:45:44 -0800

    RCI: update query_setting and query_state to include more dynamic data (our IP, iDigi hostname)

--- query_setting.py
+++ query_setting.py
@@ -18,97 +18,106 @@
 #
 # Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
 
+# DEVNOTES
+# socket.getfqdn() for my hostname
+# socket.inet_ntoa and .inet_aton exist for converting packed 4-byte struct in_addr to/from dotted quad
+# socket.getsockname() to get the local address; returns a (host, port) pair
+
+
 from simulator_settings import settings
 
 def query_setting():
-    return """
-        <query_setting>
-          <boot>
-            <eth_speed>auto</eth_speed>
-            <eth_duplex>auto</eth_duplex>
-            <dhcp>on</dhcp>
-            <ip>10.40.18.118</ip>
-            <subnet>255.255.255.0</subnet>
-            <gateway>10.40.18.1</gateway>
-            <autoip>on</autoip>
-            <addp>off</addp>
-            <static>on</static>
-          </boot>
-          <system>
-            <contact>Michael.Sutherland@digi.com
-            </contact>
-            <location>Davis, CA, U.S.A.</location>
-            <description>%s</description>
-          </system>
-          <devicesecurity>
-            <identityVerificationForm>simple</identityVerificationForm>
-            <discoveryCodingScheme>noneNone</discoveryCodingScheme>
-            <messagePassingScheme>noneNone</messagePassingScheme>
-            <clientKeySize>128Bit</clientKeySize>
-          </devicesecurity>
-          <mgmtglobal>
-            <deviceid>0x%s
-            </deviceid>
-            <rciCompressionEnabled>off</rciCompressionEnabled>
-            <tcpNodelayEnabled>off</tcpNodelayEnabled>
-            <tcpKeepalivesEnabled>off</tcpKeepalivesEnabled>
-            <connIdleTimeout>0</connIdleTimeout>
-            <dataServiceEnabled>on</dataServiceEnabled>
-            <dataServicePort>80</dataServicePort>
-            <dataServiceSecurePort>443</dataServiceSecurePort>
-            <dataServiceUrl>/ws/device</dataServiceUrl>
-          </mgmtglobal>
-          <mgmtnetwork>
-            <networkType>modemPPP</networkType>
-            <connectMethod>mt</connectMethod>
-            <mtRxKeepAlive>16</mtRxKeepAlive>
-            <mtTxKeepAlive>16</mtTxKeepAlive>
-            <mtWaitCount>3</mtWaitCount>
-            <sslValidatePeer>off</sslValidatePeer>
-          </mgmtnetwork>
-          <mgmtnetwork index="2">
-            <networkType>ethernet</networkType>
-            <connectMethod>0x00A0 len=1164 mt</connectMethod>
-            <mtRxKeepAlive>16</mtRxKeepAlive>
-            <mtTxKeepAlive>16</mtTxKeepAlive>
-            <mtWaitCount>3</mtWaitCount>
-            <sslValidatePeer>off</sslValidatePeer>
-          </mgmtnetwork>
-          <mgmtnetwork index="3">
-            <networkType>802.11</networkType>
-            <connectMethod>mt</connectMethod>
-            <mtRxKeepAlive>16</mtRxKeepAlive>
-            <mtTxKeepAlive>16</mtTxKeepAlive>
-            <mtWaitCount>3</mtWaitCount>
-            <sslValidatePeer>off</sslValidatePeer>
-          </mgmtnetwork>
-          <mgmtconnection>
-            <connectionType>client</connectionType>
-            <connectionEnabled>on</connectionEnabled>
-            <serverArray>
-              <serverAddress>en://sd1-na.idigi.com
-              </serverAddress>
-              <securitySettingsIndex>0</securitySettingsIndex>
-            </serverArray>
-          </mgmtconnection>
-          <mgmtconnection index="2">
-            <connectionType>timed</connectionType>
-            <connectionEnabled>off</connectionEnabled>
-          </mgmtconnection>
-            <mgmtconnection index="3">
-              <connectionType>serverInitiated</connectionType>
-              <connectionEnabled>off</connectionEnabled>
-            </mgmtconnection>
-            <interface name="eth0">
-              <eth_speed>auto</eth_speed>
-              <eth_duplex>auto</eth_duplex>
-              <dhcp>on</dhcp>
-              <ip>10.40.18.118</ip>
-              <subnet>255.255.255.0</subnet>
-              <gateway>10.40.18.1</gateway>
-              <autoip>on</autoip>
-              <addp>off</addp>
-              <static>on</static>
-            </interface>
-        </query_setting>
-    """ % (settings["device_name"], settings["device_id"].replace("-", ""))
+    try:
+        return """
+            <query_setting>
+              <boot>
+                <eth_speed>auto</eth_speed>
+                <eth_duplex>auto</eth_duplex>
+                <dhcp>on</dhcp>
+                <ip>%s</ip>
+                <subnet>255.255.255.0</subnet>
+                <gateway>10.40.18.1</gateway>
+                <autoip>on</autoip>
+                <addp>off</addp>
+                <static>on</static>
+              </boot>
+              <system>
+                <contact>Michael.Sutherland@digi.com
+                </contact>
+                <location>Davis, CA, U.S.A.</location>
+                <description>%s</description>
+              </system>
+              <devicesecurity>
+                <identityVerificationForm>simple</identityVerificationForm>
+                <discoveryCodingScheme>noneNone</discoveryCodingScheme>
+                <messagePassingScheme>noneNone</messagePassingScheme>
+                <clientKeySize>128Bit</clientKeySize>
+              </devicesecurity>
+              <mgmtglobal>
+                <deviceid>0x%s
+                </deviceid>
+                <rciCompressionEnabled>off</rciCompressionEnabled>
+                <tcpNodelayEnabled>off</tcpNodelayEnabled>
+                <tcpKeepalivesEnabled>off</tcpKeepalivesEnabled>
+                <connIdleTimeout>0</connIdleTimeout>
+                <dataServiceEnabled>on</dataServiceEnabled>
+                <dataServicePort>80</dataServicePort>
+                <dataServiceSecurePort>443</dataServiceSecurePort>
+                <dataServiceUrl>/ws/device</dataServiceUrl>
+              </mgmtglobal>
+              <mgmtnetwork>
+                <networkType>modemPPP</networkType>
+                <connectMethod>mt</connectMethod>
+                <mtRxKeepAlive>16</mtRxKeepAlive>
+                <mtTxKeepAlive>16</mtTxKeepAlive>
+                <mtWaitCount>3</mtWaitCount>
+                <sslValidatePeer>off</sslValidatePeer>
+              </mgmtnetwork>
+              <mgmtnetwork index="2">
+                <networkType>ethernet</networkType>
+                <connectMethod>0x00A0 len=1164 mt</connectMethod>
+                <mtRxKeepAlive>16</mtRxKeepAlive>
+                <mtTxKeepAlive>16</mtTxKeepAlive>
+                <mtWaitCount>3</mtWaitCount>
+                <sslValidatePeer>off</sslValidatePeer>
+              </mgmtnetwork>
+              <mgmtnetwork index="3">
+                <networkType>802.11</networkType>
+                <connectMethod>mt</connectMethod>
+                <mtRxKeepAlive>16</mtRxKeepAlive>
+                <mtTxKeepAlive>16</mtTxKeepAlive>
+                <mtWaitCount>3</mtWaitCount>
+                <sslValidatePeer>off</sslValidatePeer>
+              </mgmtnetwork>
+              <mgmtconnection>
+                <connectionType>client</connectionType>
+                <connectionEnabled>on</connectionEnabled>
+                <serverArray>
+                  <serverAddress>en://%s
+                  </serverAddress>
+                  <securitySettingsIndex>0</securitySettingsIndex>
+                </serverArray>
+              </mgmtconnection>
+              <mgmtconnection index="2">
+                <connectionType>timed</connectionType>
+                <connectionEnabled>off</connectionEnabled>
+              </mgmtconnection>
+                <mgmtconnection index="3">
+                  <connectionType>serverInitiated</connectionType>
+                  <connectionEnabled>off</connectionEnabled>
+                </mgmtconnection>
+                <interface name="eth0">
+                  <eth_speed>auto</eth_speed>
+                  <eth_duplex>auto</eth_duplex>
+                  <dhcp>on</dhcp>
+                  <ip>10.40.18.118</ip>
+                  <subnet>255.255.255.0</subnet>
+                  <gateway>10.40.18.1</gateway>
+                  <autoip>on</autoip>
+                  <addp>off</addp>
+                  <static>on</static>
+                </interface>
+            </query_setting>
+        """ % (settings.get('my_ipaddress','0.0.0.0'), settings["device_name"], settings["device_id"].replace("-", ""), settings['idigi_server'])
+    except Exception, e:
+        print "query_setting: %s" % str(e)
