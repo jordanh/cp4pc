f0f91f2f5d3167c97814c91c199b394bc276ae60
Jordan Husney <jordan.husney@gmail.com>
2012-04-18 08:22:22 -0500

    Added/modified copyright statements and placed files under Mozilla v2.0 license.

--- diag.py
+++ diag.py
@@ -1,6 +1,17 @@
+#
+# Copyright (c) 2009-2012 Digi International Inc.
+# All rights not expressly granted are reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+# 
+# Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+#
 """
-Simple Diagnostic Tool for Xbee management on the connectport
+Simple Diagnostic Tool for XBee management on the connectport
 """
+
 import zigbee
 import struct
 import sys
--- idigidata.py
+++ idigidata.py
@@ -1,3 +1,14 @@
+#
+# Copyright (c) 2009-2012 Digi International Inc.
+# All rights not expressly granted are reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+# 
+# Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+#
+
 #send_to_idigi (data, filename, [collection, content_type, archive=False, append=False, timeout]) -> (success, error, errmsg)
 #
 #Send data to the iDigi data service and wait for the response.
