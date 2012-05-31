3fa0999e8b7d04231709a5957a15bc922dbb6195
Michael Sutherland <Michael.Sutherland@digi.com>
2012-05-31 14:23:40 -0700

    cp4pc: changed default COM port to be empty string (not all PCs have COM1, who knew?).

--- simulator_settings.py
+++ simulator_settings.py
@@ -103,7 +103,7 @@ if 'mac' not in settings:
 settings.setdefault('version', "0.0.0") #I'd recommend keeping this format
 
 # serial port settings for XBee
-settings.setdefault('com_port', 'COM1') #default to first serial port.
+settings.setdefault('com_port', '') #default to empty string
 settings.setdefault('baud', 115200) #should the default be 9600?
 
 # iDigi Settings
